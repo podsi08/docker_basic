9a873b846110435d8e351eb0855232f5c5633440
podsi08 <aleksandra.tobiszewska08@gmail.com>
2019-05-13 13:19:43 +0200

    change port, add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+#Building an image
+`docker build -t docker-basic`
+
+#Run image
+`docker run -p 3002:3002 docker-basic .`
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const PORT = 3000;
+const PORT = 3002;
 
 const app = express();
 
@@ -8,4 +8,4 @@ app.get('/', (req, res) => {
   res.send('Hello world!!!');
 });
 
-app.listen(PORT, () => { console.log('Listening on port 3000!') });
+app.listen(PORT, () => { console.log(`Listening on port ${PORT}!`) });
