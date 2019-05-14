3c519448baa5fa29936c8f0ec5b5783ebe5c6b0b
podsi08 <aleksandra.tobiszewska08@gmail.com>
2019-05-14 13:13:46 +0200

    add slow and error endpoint, add pm2 commands

--- server.js
+++ server.js
@@ -8,4 +8,18 @@ app.get('/', (req, res) => {
   res.send('Hello world!!!');
 });
 
+app.get('/exec', (req, res) => {
+  throw new Error('Exception occured!');
+});
+
+app.get('/slow_endpoint', (req, res) => {
+  const n = 400;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j< n; j++) {
+      console.log(`Iter ${i}.${j}`);
+    }
+  }
+  res.send('Iterate');
+});
+
 app.listen(PORT, () => { console.log(`Listening on port ${PORT}!`) });
